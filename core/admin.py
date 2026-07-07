f6d85b8c29c655cf60e5b3cf23198b1032cb70dd
bhumit23 <bhumitvaghela71@gmail.com>
2026-07-07 22:18:17 +0530

    Register models in Django Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import UserProfile, WeatherHistory, MandiPrice
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'state', 'primary_crop')
+
+@admin.register(WeatherHistory)
+class WeatherHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'query_location', 'temperature', 'condition', 'searched_at')
+    
+@admin.register(MandiPrice)
+class MandiPriceAdmin(admin.ModelAdmin):
+    list_display = ('crop_name', 'mandi_name', 'today_price', 'price_date')
+    list_filter = ('crop_name', 'price_date')
--- crops/admin.py
+++ crops/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Crop
+from .models import Crop, DiseasePrediction, CropLibrary, FertilizerRecommendation, FarmingNews
 
 @admin.register(Crop)
 class CropAdmin(admin.ModelAdmin):
@@ -7,3 +7,18 @@ class CropAdmin(admin.ModelAdmin):
     list_filter = ('status', 'name')
     search_fields = ('variety', 'farmer__username')
 
+@admin.register(DiseasePrediction)
+class DiseasePredictionAdmin(admin.ModelAdmin):
+    list_display = ('crop_name', 'farmer', 'disease', 'confidence', 'created_at')
+
+@admin.register(CropLibrary)
+class CropLibraryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gujarati_name', 'season')
+
+@admin.register(FertilizerRecommendation)
+class FertilizerRecommendationAdmin(admin.ModelAdmin):
+    list_display = ('crop_name', 'farmer', 'fertilizer_name', 'crop_stage')
+
+@admin.register(FarmingNews)
+class FarmingNewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source_name', 'pub_date')
